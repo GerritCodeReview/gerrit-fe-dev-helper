091a38fd64acd6670f6aff2a35658f3801fc70b5
Ben Rohlfs <brohlfs@google.com>
2023-11-27 13:16:31 +0100

    Migrate to a service worker
    Change-Id: I583b0ff70a414c33c66781846d866e7784460011

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,32 +1,32 @@
-const path = require('path');
-const srcDir = '../src/';
+const path = require("path");
+const srcDir = "../src/";
 
 const MODE = "production";
 
 module.exports = {
-    mode: MODE,
-    entry: {
-        popup: path.join(__dirname, srcDir + 'popup.ts'),
-        background: path.join(__dirname, srcDir + 'background.ts'),
-        content_script: path.join(__dirname, srcDir + 'content_script.ts')
-    },
-    optimization:{
-        minimize: MODE === "production",
-    },
-    output: {
-        path: path.join(__dirname, '../dist/'),
-        filename: '[name].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    }
+  mode: MODE,
+  entry: {
+    popup: path.join(__dirname, srcDir + "popup.ts"),
+    service_worker: path.join(__dirname, srcDir + "service_worker.ts"),
+    content_script: path.join(__dirname, srcDir + "content_script.ts"),
+  },
+  optimization: {
+    minimize: MODE === "production",
+  },
+  output: {
+    path: path.join(__dirname, "../dist/"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
 };
