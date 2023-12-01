53009025298e0e76639e0267961868e1b96d4355
Ben Rohlfs <brohlfs@google.com>
2023-12-01 19:08:04 +0100

    Migrate to Manifest V3: Get it working
    Change-Id: I72e35dc2c05954894d5d07a9a00240afc5689c54

--- src/storage.ts
+++ src/storage.ts
@@ -2,61 +2,10 @@ import {DEFAULT_RULES, Rule, isValidRule} from './utils';
 
 type TabsEnabled = {[tabId: string]: boolean};
 
-export class Storage {
-  private cachedActiveTabId: number = 0;
-  private cachedTabsEnabled: TabsEnabled = {};
-  private cachedRules: Rule[] = [...DEFAULT_RULES];
-
-  constructor() {
-    this.init();
-  }
-
-  private async init() {
-    this.cachedActiveTabId = await this.getActiveTabIdAsync();
-    this.cachedTabsEnabled = await this.getTabsEnabledAsync();
-    this.cachedRules = await this.getRulesAsync();
-
-    chrome.storage.onChanged.addListener((changes, namespace) => {
-      for (let [key, {oldValue, newValue}] of Object.entries(changes)) {
-        if (namespace === 'sync' && key === 'rules') {
-          this.cachedRules = newValue;
-        }
-        if (namespace === 'session' && key === 'tabsEnabled') {
-          this.cachedTabsEnabled = newValue;
-        }
-        if (namespace === 'session' && key === 'activeTabId') {
-          this.cachedActiveTabId = newValue;
-        }
-        console.log(
-          `Storage key "${key}" in namespace "${namespace}" changed.`,
-          `Old value was "${oldValue}", new value is "${newValue}".`
-        );
-      }
-    });
-  }
-
-  // ACTIVE TAB ID
-
-  getActiveTabIdCached(): number {
-    return this.cachedActiveTabId;
-  }
-
-  async getActiveTabIdAsync(): Promise<number> {
-    const data = await chrome.storage.session.get('activeTabId');
-    return (data?.['activeTabId'] as number) ?? 0;
-  }
-
-  async setActiveTabId(activeTabId: number) {
-    await chrome.storage.session.set({activeTabId});
-  }
-
+export class StorageUtil {
   // RULES
 
-  getRulesCached(): Rule[] {
-    return this.cachedRules;
-  }
-
-  async getRulesAsync(): Promise<Rule[]> {
+  async getRules(): Promise<Rule[]> {
     const data = await chrome.storage.sync.get('rules');
     return (data?.['rules'] as Rule[]) ?? [...DEFAULT_RULES];
   }
@@ -65,8 +14,8 @@ export class Storage {
     await chrome.storage.sync.set({rules});
   }
 
-  async validateRules() {
-    const existingRules = await this.getRulesAsync();
+  async initRules() {
+    const existingRules = await this.getRules();
     const validRules = existingRules.filter(isValidRule);
     if (!validRules.length) {
       await this.setRules([...DEFAULT_RULES]);
@@ -77,28 +26,29 @@ export class Storage {
 
   // TABS ENABLED
 
-  async getTabsEnabledAsync(): Promise<TabsEnabled> {
+  async getTabsEnabled(): Promise<TabsEnabled> {
     const data = await chrome.storage.session.get('tabsEnabled');
     return (data?.['tabsEnabled'] as TabsEnabled) ?? {};
   }
 
-  private async setTabsEnabled(tabsEnabled: TabsEnabled) {
+  async setTabsEnabled(tabsEnabled: TabsEnabled) {
     await chrome.storage.session.set({tabsEnabled});
   }
 
   async setTabEnabled(tabId: number) {
-    const tabsEnabled: TabsEnabled = await this.getTabsEnabledAsync();
+    const tabsEnabled: TabsEnabled = await this.getTabsEnabled();
     tabsEnabled[`${tabId}`] = true;
     await this.setTabsEnabled(tabsEnabled);
   }
 
   async setTabDisabled(tabId: number) {
-    const tabsEnabled: TabsEnabled = await this.getTabsEnabledAsync();
+    const tabsEnabled: TabsEnabled = await this.getTabsEnabled();
     delete tabsEnabled[`${tabId}`];
     await this.setTabsEnabled(tabsEnabled);
   }
 
-  async isTabEnabledCached(tabId: number) {
-    return this.cachedTabsEnabled[`${tabId}`] === true;
+  async isTabEnabled(tabId: number) {
+    const tabsEnabled: TabsEnabled = await this.getTabsEnabled();
+    return tabsEnabled[`${tabId}`] === true;
   }
 }
