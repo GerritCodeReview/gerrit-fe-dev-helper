f91f36cca5d32de9ed2fe2c43ee8dacb8f102a53
Ben Rohlfs <brohlfs@google.com>
2023-12-01 22:53:23 +0100

    Remove support for injecting arbitrary JS and HTML plugins
    HTML plugins don't exist anymore for quite a while. That was just
    obsolete.
    
    Injecting arbitrary JS code snippets is not allowed in Manifest V3
    extensions. The origin cannot be validated.
    
    Change-Id: I6f58cd01fbb53be38baefe8c3914c786c9d2dcdb

--- src/utils.ts
+++ src/utils.ts
@@ -23,8 +23,6 @@ export function isInjectRule(rule: Rule) {
     Operator.INJECT_JS_MODULE_PLUGIN,
     Operator.INJECT_JS_PLUGIN,
     Operator.INJECT_HTML_CODE,
-    Operator.INJECT_HTML_PLUGIN,
-    Operator.INJECT_JS_CODE,
     Operator.INJECT_EXP,
   ].some(op => op === rule.operator);
 }
@@ -166,11 +164,9 @@ export function convertOperatorToType(
 export enum Operator {
   BLOCK = 'block',
   REDIRECT = 'redirect',
-  INJECT_HTML_PLUGIN = 'injectHtmlPlugin',
   INJECT_HTML_CODE = 'injectHtmlCode',
   INJECT_JS_PLUGIN = 'injectJSPlugin',
   INJECT_JS_MODULE_PLUGIN = 'injectJSModule',
-  INJECT_JS_CODE = 'injectJSCode',
   REMOVE_RESPONSE_HEADER = 'rRespHeader',
   ADD_RESPONSE_HEADER = 'addRespHeader',
   ADD_REQUEST_HEADER = 'addReqHeader',
